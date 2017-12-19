11c2e4d1534adb3c6798db0eeda2ba660e57ec62
Neal Lester <neal@3dsafety.com>
2017-12-19 09:39:33 -0800

    Added Eq to trait object EntityStruct

--- src/lib.rs
+++ src/lib.rs
@@ -13,49 +13,55 @@ struct Batch {
 
 struct Token {}
 
-// type EntityStructImp = Clone + Eq + Serialize + DeserializeOwned;
+pub trait EntityStructClone {
 
-//pub trait EntityStructImp: Clone + Eq + Serialize + DeserializeOwned {}
-//
-//pub struct Entity<'a, E> where E: Sized + Clone + Eq + Serialize + DeserializeOwned {
-//
-//    item: E,
-//    id: Uuid,
-//    base_version: u32,
-//    toekn: Option<&'a Token>,
-//    return_channel: Sender<Entity<'a, E>>,
-//
-//}
-
-
-pub trait EntityStructImp {
-
-    fn clone_entity (&self) -> Self where Self: Sized;
+    fn entity_clone (&self) -> Self where Self: Sized;
 
 }
 
-impl<T: Clone> EntityStructImp for T {
+pub trait EntityStructEq {
 
-    fn clone_entity (&self) -> T {
-        return self.clone()
+    fn entity_eq (&self, other: &Self) -> bool where Self: Sized;
+}
+
+impl<C: Clone> EntityStructClone for C {
+
+    fn entity_clone (&self) -> C { return self.clone() }
+
+}
+
+impl<E: Eq> EntityStructEq for E {
+
+    fn entity_eq (&self, other: &Self) -> bool {
+        return self.eq(other)
     }
 }
 
-pub trait EntityStruct: EntityStructImp {}
+pub trait EntityStruct: EntityStructClone + EntityStructEq {}
+// This indirection ^ prevents an implementation conflict for descendants
+//                  |
+//error[E0119]: conflicting implementations of trait `EntityStruct` for type `MyStruct`:
+//--> src/lib.rs:49:1
+//|
+//37 | / impl<T: Clone> EntityStruct for T {
+//    38 | |
+//    39 | |     fn clone_entity (&self) -> T {
+//        40 | |         return self.clone()
+//        41 | |     }
+//    42 | | }
+//| |_- first implementation here
+//...
+//49 |   impl EntityStruct for MyStruct {}
+//|   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ conflicting implementation for `MyStruct`
 
-#[derive(Clone)]
-pub struct MyStruct {}
-
-impl EntityStruct for MyStruct {}
 
 
-
-pub struct Entity<'a, E: ?Sized> where E: EntityStruct {
+pub struct Entity<'a, E: ?Sized> where E: EntityStructClone {
 
     item: Box<E>,
     id: Uuid,
     base_version: u32,
-    toekn: Option<&'a Token>,
+    token: Option<&'a Token>,
     return_channel: Sender<Entity<'a, E>>,
 
 }
@@ -73,14 +79,20 @@ mod tests {
     use serde::Serialize;
     use serde::de::DeserializeOwned;
 
+    #[derive(Clone, PartialEq, Eq)]
+    pub struct MyStruct {
+        a: i32,
+        v: Vec<i32>,
+    }
+
+    impl EntityStruct for MyStruct {   }
+
 
 
     #[test]
     fn it_works() {
         let (tx, rx): (Sender<Entity<EntityStruct>>, Receiver<Entity<EntityStruct>>) = channel();
         let v: Vec<Box<Entity<EntityStruct>>> =  Vec::new();
-
-
 
         assert_eq!(2 + 2, 4);
     }
