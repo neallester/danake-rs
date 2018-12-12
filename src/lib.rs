399bb2faba1d2444c2af49df5eedb3e1417142c1
Neal Lester <neal@3dsafety.com>
2018-12-11 17:44:09 -0800

    Starting over; compiles without warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,86 @@
 mod storage;
 
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
+//#[macro_use]
+//extern crate serde_derive;
+//extern crate serde;
+//extern crate serde_json;
 extern crate uuid;
-extern crate erased_serde;
+//extern crate erased_serde;
 use self::uuid::Uuid;
-use erased_serde::{Serialize, Serializer};
-use std::sync::mpsc::Sender;
-use std::result::Result;
-use std::cell::RefCell;
-use std::cell::RefMut;
+//use erased_serde::{Serialize, Serializer};
+//use std::sync::mpsc::Sender;
+//use std::result::Result;
+//use std::cell::RefCell;
+//use std::cell::RefMut;
 
 
-pub trait EntityStructClone {
+#[derive(Debug)]
+pub struct Entity {
 
-    fn entity_clone (&self) -> Self where Self: Sized;
-
-}
-
-pub trait EntityStructEq {
-
-    fn entity_eq (&self, other: &Self) -> bool where Self: Sized ;
-}
-
-pub trait EntityStructSerialize {
-
-    fn entity_serialize<'a> (&self, serializer: Box<Serializer + 'a>) -> Result<(), erased_serde::Error>;
-
-    fn to_json(&self) -> Result<std::string::String, serde_json::Error>;
-}
-
-impl<C: Clone> EntityStructClone for C {
-
-    fn entity_clone (&self) -> C { return self.clone() }
+//    item: Option<Box<RefCell<E>>>,
+    id: Uuid,
+    version: u32,
 
 }
 
-impl<E: Eq> EntityStructEq for E {
 
-    fn entity_eq (&self, other: &Self) -> bool{
-        return self.eq(other)
-    }
-}
+//pub trait EntityStructClone {
+//
+//    fn entity_clone (&self) -> Self where Self: Sized;
+//
+//}
+//
+//pub trait EntityStructEq {
+//
+//    fn entity_eq (&self, other: &Self) -> bool where Self: Sized ;
+//}
+//
+//pub trait EntityStructSerialize {
+//
+//    fn entity_serialize<'a> (&self, serializer: Box<Serializer + 'a>) -> Result<(), erased_serde::Error>;
+//
+//    fn to_json(&self) -> Result<std::string::String, serde_json::Error>;
+//}
+//
+//impl<C: Clone> EntityStructClone for C {
+//
+//    fn entity_clone (&self) -> C { return self.clone() }
+//
+//}
+//
+//impl<E: Eq> EntityStructEq for E {
+//
+//    fn entity_eq (&self, other: &Self) -> bool{
+//        return self.eq(other)
+//    }
+//}
+//
+//impl<S: serde::Serialize> EntityStructSerialize for S
+//{
+//    fn entity_serialize<'a> (&self, mut serializer: Box<Serializer + 'a>) -> Result<(), erased_serde::Error> {
+//        let result = self.erased_serialize(&mut *serializer);
+//        match result {
+//            Ok(_) => Ok(()),
+//            Err(err) => Err (err),
+//        }
+//    }
+//
+//    fn to_json(&self) -> Result<std::string::String, serde_json::Error> {
+//        serde_json::to_string (self)
+//    }
+//
+//}
+//
+//pub trait AsEntityStruct<'a> {
+//
+//    fn as_entity_struct (&'a self) -> &'a EntityStruct;
+//
+//}
+//
+//pub trait EntityStruct<'a>: Send + Sync + EntityStructClone + EntityStructEq + EntityStructSerialize + AsEntityStruct<'a> {
+//
+//}
 
-impl<S: serde::Serialize> EntityStructSerialize for S
-{
-    fn entity_serialize<'a> (&self, mut serializer: Box<Serializer + 'a>) -> Result<(), erased_serde::Error> {
-        let result = self.erased_serialize(&mut *serializer);
-        match result {
-            Ok(_) => Ok(()),
-            Err(err) => Err (err),
-        }
-    }
-
-    fn to_json(&self) -> Result<std::string::String, serde_json::Error> {
-        serde_json::to_string (self)
-    }
-
-}
-
-pub trait EntityStruct<'a>: Send + Sync + EntityStructClone + EntityStructEq + EntityStructSerialize {
-
-    fn as_entity_struct (&'a self) -> &'a EntityStruct;
-
-}
 // This indirection ^ prevents an implementation conflict for descendants
 //                  |
 //error[E0119]: conflicting implementations of trait `EntityStruct` for type `MyStruct`:
@@ -83,48 +98,36 @@ pub trait EntityStruct<'a>: Send + Sync + EntityStructClone + EntityStructEq + E
 //|   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ conflicting implementation for `MyStruct`
 
 
-impl <'a, S: Sized + Send + Sync + Eq + Clone + serde::Serialize + EntityStructClone + EntityStructEq + EntityStructSerialize>EntityStruct<'a> for S {
+//impl <'a, S: Sized + Send + Sync + Eq + Clone + serde::Serialize + EntityStructClone + EntityStructEq + EntityStruct<'a> + AsEntityStruct<'a> + EntityStructSerialize>EntityStruct<'a> for S {
 
-    fn as_entity_struct (&'a self) -> &'a EntityStruct {
-        return self as &'a EntityStruct;
-    }
+//    fn as_entity_struct (&'a self) -> &'a EntityStruct {
+//        return self as &'a EntityStruct;
+//    }
 
-}
-
-#[derive(Debug)]
-pub struct Entity<'a, E: ?Sized> where E: 'a + EntityStruct<'a> {
-
-    item: Option<Box<RefCell<E>>>,
-    id: Uuid,
-    base_version: u32,
-    return_channel: Option<Sender<Entity<'a, EntityStruct<'a>>>>,
-    // TODO return channel needs to work on an EntityStruct, not parameter E so that we can have
-    //      one receiver for all entity types
-    // TODO id should be a reference instead of the struct, with the owner being the batch object.
-
-}
-
-impl<'a, E> Drop for Entity<'a, E> where E: 'a + ?Sized + EntityStruct<'a> {
-    fn drop(&mut self) {
-        let sender = self.return_channel.take();
-        match sender {
-            Some (sender) => {
-                let item = self.item.take();
-                let return_entity = Entity {
-                    item: item,
-                    id: self.id.clone(),
-                    base_version: self.base_version.clone(),
-                    return_channel: None,
-                };
-                println! ("Sending return");
-                let _ = sender.send(return_entity);
-            },
-            None => {}
-        }
+//}
 
 
-    }
-}
+//impl<'a, E> Drop for Entity<'a, E> where E: 'a + ?Sized + EntityStruct<'a> {
+//    fn drop(&mut self) {
+//        let sender = self.return_channel.take();
+//        match sender {
+//            Some (sender) => {
+//                let item = self.item.unwrap().get_mut().as_entity_struct();
+//                let item = self.item.take() as Option<Box<RefCell<EntityStruct>>>;
+//                let return_entity = Entity {
+//                    item: item,
+//                    id: self.id.clone(),
+//                    base_version: self.base_version.clone(),
+//                    return_channel: None,
+//                };
+//                let _ = sender.send(return_entity);
+//            },
+//            None => {}
+//        }
+//
+//
+//    }
+//}
 
 // Example of the compilation problem I am having
 //use std::sync::mpsc::Sender;
@@ -198,30 +201,30 @@ impl<'a, E> Drop for Entity<'a, E> where E: 'a + ?Sized + EntityStruct<'a> {
 // Use a helper function EntityStruct.asTrait(&self) -> EntityStruct
 // Which can be provided a default implementation for all Sized
 
-impl<'a, E> Entity<'a, E> where E: ?Sized + EntityStruct<'a> {
-
-    fn borrow_mut (&mut self) ->  RefMut<E> {
-        match self.item {
-            Some (ref mut item) => {
-                let item = &**item;
-                let item = item.borrow_mut();
-                return item;
-            }
-            None => panic!("danake::Entity without Item"),
-        }
-    }
-
-    fn item_json(&self) -> Result<std::string::String, serde_json::Error> {
-        match self.item {
-            Some (ref item) => {
-                let item = &**item;
-                let item = item.borrow_mut();
-                item.to_json()
-            }
-            None => Ok (String::from("{}")),
-        }
-    }
-}
+//impl<'a, E> Entity<'a, E> where E: ?Sized + EntityStruct<'a> {
+//
+//    fn borrow_mut (&mut self) ->  RefMut<E> {
+//        match self.item {
+//            Some (ref mut item) => {
+//                let item = &**item;
+//                let item = item.borrow_mut();
+//                return item;
+//            }
+//            None => panic!("danake::Entity without Item"),
+//        }
+//    }
+//
+//    fn item_json(&self) -> Result<std::string::String, serde_json::Error> {
+//        match self.item {
+//            Some (ref item) => {
+//                let item = &**item;
+//                let item = item.borrow_mut();
+//                item.to_json()
+//            }
+//            None => Ok (String::from("{}")),
+//        }
+//    }
+//}
 
 
 #[cfg(test)]
@@ -252,76 +255,76 @@ mod tests {
     }
 
 
-    fn mock_test_entity<'a> (uuid: Uuid, sender: Sender<Entity<'a, EntityStruct>>) -> Entity<'a, TestStruct> {
-        let s1 = TestStruct {
-            a: 100,
-            v: vec![200, 300],
-        };
-        Entity {
-            item: Some(Box::new(RefCell::new(s1))),
-            id: uuid.clone(),
-            base_version: 0,
-            return_channel: Some(sender),
-        }
-    }
+//    fn mock_test_entity<'a> (uuid: Uuid, sender: Sender<Entity<'a, EntityStruct>>) -> Entity<'a, TestStruct> {
+//        let 1 = TestStruct {
+//            a: 100,
+//            v: vec![200, 300],
+//        };
+//        Entity {
+//            item: Some(Box::new(RefCell::new(s1))),
+//            id: uuid.clone(),
+//            base_version: 0,
+//            return_channel: Some(sender),
+//        }
+//    }
 
     #[test]
     fn entity_test() {
         let uuid = Uuid::new_v4();
         let uuid_thread = uuid.clone();
         let (tx, rx): (Sender<Entity<EntityStruct>>, Receiver<Entity<EntityStruct>>) = channel();
-        thread::spawn (move || {
-            let mut my_entity = mock_test_entity(uuid_thread, tx);
-            {
-                let item1 = my_entity.borrow_mut();
-                assert_eq!(100, item1.a);
-            }
-            assert_eq!(100, my_entity.borrow_mut().a);
-            let mut item = my_entity.borrow_mut();
-            assert_eq!(100, item.a);
-            item.a = 200;
-            assert_eq!(200, item.a);
-        });
-        let returned_entity = rx.recv();
-        assert_eq!("{\"a\":200,\"v\":[200,300]}", returned_entity.unwrap().item_json().unwrap());
+//        thread::spawn (move || {
+//            let mut my_entity = mock_test_entity(uuid_thread, tx);
+//            {
+//                let item1 = my_entity.borrow_mut();
+//                assert_eq!(100, item1.a);
+//            }
+//            assert_eq!(100, my_entity.borrow_mut().a);
+//            let mut item = my_entity.borrow_mut();
+//            assert_eq!(100, item.a);
+//            item.a = 200;
+//            assert_eq!(200, item.a);
+//        });
+//        let returned_entity = rx.recv();
+//        assert_eq!("{\"a\":200,\"v\":[200,300]}", returned_entity.unwrap().item_json().unwrap());
     }
 
-    #[test]
-    fn entity_struct_test () {
-        let s1 = TestStruct {
-            a: 100,
-            v: vec![200, 300],
-        };
-        let s2 = s1.entity_clone();
-        assert_eq!(s1, s2);
-        assert! (s1.entity_eq (&s2));
-        assert_eq!(100, s1.a);
-        assert_eq!(2, s1.v.len());
-        assert_eq!(&200, s1.v.get(0).unwrap());
-        assert_eq!(&300, s1.v.get(1).unwrap());
-        assert_eq!(100, s2.a);
-        assert_eq!(2, s2.v.len());
-        assert_eq!(&200, s2.v.get(0).unwrap());
-        assert_eq!(&300, s2.v.get(1).unwrap());
-        let s3 = TestStruct {
-            a: 200,
-            v: vec![200, 300],
-        };
-        assert_ne! (s1, s3);
-        assert! (!s1.entity_eq (&s3));
-        let s4 = TestStruct {
-            a: 100,
-            v: vec![500, 300],
-        };
-        assert_ne! (s1, s4);
-        assert! (!s1.entity_eq (&s4));
-
-        assert_eq! ("{\"a\":100,\"v\":[200,300]}", s1.to_json().unwrap());
-
-        let bytes = Vec::new();
-        let mut json = Rc::new (self::serde_json::Serializer::new(bytes));
-        let json_ref = Rc::get_mut(&mut json).unwrap();
-        let erased: Box<Serializer> = Box::new(Serializer::erase(json_ref));
-        let _ = s1.entity_serialize(erased);
-    }
+//    #[test]
+//    fn entity_struct_test () {
+//        let s1 = TestStruct {
+//            a: 100,
+//            v: vec![200, 300],
+//        };
+//        let s2 = s1.entity_clone();
+//        assert_eq!(s1, s2);
+//        assert! (s1.entity_eq (&s2));
+//        assert_eq!(100, s1.a);
+//        assert_eq!(2, s1.v.len());
+//        assert_eq!(&200, s1.v.get(0).unwrap());
+//        assert_eq!(&300, s1.v.get(1).unwrap());
+//        assert_eq!(100, s2.a);
+//        assert_eq!(2, s2.v.len());
+//        assert_eq!(&200, s2.v.get(0).unwrap());
+//        assert_eq!(&300, s2.v.get(1).unwrap());
+//        let s3 = TestStruct {
+//            a: 200,
+//            v: vec![200, 300],
+//        };
+//        assert_ne! (s1, s3);
+//        assert! (!s1.entity_eq (&s3));
+//        let s4 = TestStruct {
+//            a: 100,
+//            v: vec![500, 300],
+//        };
+//        assert_ne! (s1, s4);
+//        assert! (!s1.entity_eq (&s4));
+//
+//        assert_eq! ("{\"a\":100,\"v\":[200,300]}", s1.to_json().unwrap());
+//
+//        let bytes = Vec::new();
+//        let mut json = Rc::new (self::serde_json::Serializer::new(bytes));
+//        let json_ref = Rc::get_mut(&mut json).unwrap();
+//        let erased: Box<Serializer> = Box::new(Serializer::erase(json_ref));
+//        let _ = s1.entity_serialize(erased);
+//    }
 }
--- src/storage/mock.rs
+++ src/storage/mock.rs
@@ -1,118 +1,118 @@
 extern crate uuid;
 
-use std::collections::HashMap;
-use self::uuid::Uuid;
+//use std::collections::HashMap;
+//use self::uuid::Uuid;
 
-pub struct Storage {
-    entities: HashMap<String, HashMap<Uuid, String>>
-}
+//pub struct Storage {
+////    entities: HashMap<String, HashMap<Uuid, String>>
+//}
 
-impl<> Storage {
+//impl<> Storage {
 
-    pub fn new() -> Storage {
-        Storage {
-            entities: HashMap::new(),
-        }
-    }
+//    pub fn new() -> Storage {
+//        Storage {
+//            entities: HashMap::new(),
+//        }
+//    }
+//
+//    pub fn set(&mut self, name: &str, uuid: &Uuid, json: String) {
+//        let needs_insert = match self.entities.get_mut (name) {
+//            Some(mm) => {
+//                mm.insert(uuid.clone(), json.to_string());
+//                false
+//            },
+//            None => {
+//                true
+//            },
+//        };
+//        if needs_insert {
+//            let mut new: HashMap<Uuid, String> = HashMap::new();
+//            new.insert(uuid.clone(), json);
+//            self.entities.insert (name.to_string(), new);
+//        }
+//    }
+//
+//
+//    pub fn get (&self, name: &str, uuid: &Uuid) -> Option<String> {
+//        let map_map = self.entities.get(name);
+//        match map_map {
+//            Some (mm) => {
+//                let json = mm.get (uuid);
+//                match json {
+//                    Some (j) => {
+//                        return Some (j.clone());
+//                    },
+//                    None => return None,
+//                }
+//            }
+//            None => return None,
+//        }
+//    }
+//
+//    pub fn table_count (&self) -> usize {
+//        self.entities.len()
+//    }
+//
+//    pub fn count (&self, name: &str) -> usize {
+//        match self.entities.get (name) {
+//            Some (mm) => {
+//                mm.len()
+//            }
+//            None => 0,
+//        }
+//    }
 
-    pub fn set(&mut self, name: &str, uuid: &Uuid, json: String) {
-        let needs_insert = match self.entities.get_mut (name) {
-            Some(mm) => {
-                mm.insert(uuid.clone(), json.to_string());
-                false
-            },
-            None => {
-                true
-            },
-        };
-        if needs_insert {
-            let mut new: HashMap<Uuid, String> = HashMap::new();
-            new.insert(uuid.clone(), json);
-            self.entities.insert (name.to_string(), new);
-        }
-    }
-
-
-    pub fn get (&self, name: &str, uuid: &Uuid) -> Option<String> {
-        let map_map = self.entities.get(name);
-        match map_map {
-            Some (mm) => {
-                let json = mm.get (uuid);
-                match json {
-                    Some (j) => {
-                        return Some (j.clone());
-                    },
-                    None => return None,
-                }
-            }
-            None => return None,
-        }
-    }
-
-    pub fn table_count (&self) -> usize {
-        self.entities.len()
-    }
-
-    pub fn count (&self, name: &str) -> usize {
-        match self.entities.get (name) {
-            Some (mm) => {
-                mm.len()
-            }
-            None => 0,
-        }
-    }
-
-}
+//}
 
 
 #[cfg(test)]
 mod tests {
 
-    use storage::mock::Storage;
-    use storage::mock::uuid::Uuid;
-
-    #[test]
-    fn storage_entities_tests () {
-        let uuid0 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3850").unwrap();
-        let uuid1 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3851").unwrap();
-        let uuid2 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3852").unwrap();
-        let uuid3 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3853").unwrap();
-        let json0 = String::from ("{\"value:\":0}");
-        let json1 = String::from ("{\"value:\":1}");
-        let json2 = String::from ("{\"value:\":2}");
-        let json3 = String::from ("{\"value:\":3}");
-        let json4 = String::from ("{\"value:\":4}");
-        let json5 = String::from ("{\"value:\":5}");
-        let name0 = String::from("table0");
-        let name1 = String::from("table1");
-        let mut storage = Storage::new();
-        assert_eq!(None, storage.get (&name0, &uuid0));
-        assert_eq!(0, storage.count(&name0));
-        assert_eq!(0, storage.table_count());
-        storage.set (&name0, &uuid0, json0.clone());
-        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
-        assert_eq! (1, storage.count (&name0));
-        assert_eq!(None, storage.get (&name1, &uuid1));
-        assert_eq!(0, storage.count(&name1));
-        assert_eq!(1, storage.table_count());
-        storage.set (&name0, &uuid1, json1.clone());
-        storage.set (&name1, &uuid2, json2.clone());
-        storage.set (&name1, &uuid3, json3.clone());
-        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
-        assert_eq!(json1, storage.get (&name0, &uuid1).unwrap());
-        assert_eq! (2, storage.count (&name0));
-        assert_eq!(json2, storage.get (&name1, &uuid2).unwrap());
-        assert_eq!(json3, storage.get (&name1, &uuid3).unwrap());
-        assert_eq!(2, storage.count(&name1));
-        assert_eq!(2, storage.table_count());
-        storage.set (&name0, &uuid1, json4.clone());
-        storage.set (&name1, &uuid3, json5.clone());
-        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
-        assert_eq!(json4, storage.get (&name0, &uuid1).unwrap());
-        assert_eq! (2, storage.count (&name0));
-        assert_eq!(json2, storage.get (&name1, &uuid2).unwrap());
-        assert_eq!(json5, storage.get (&name1, &uuid3).unwrap());
-        assert_eq!(2, storage.count(&name1));
-        assert_eq!(2, storage.table_count());
-    }
+//    use storage::mock::Storage;
+//    use storage::mock::uuid::Uuid;
+//
+//    #[test]
+//    fn storage_entities_tests () {
+//        let uuid0 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3850").unwrap();
+//        let uuid1 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3851").unwrap();
+//        let uuid2 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3852").unwrap();
+//        let uuid3 = Uuid::parse_str("14e144e3-81ac-4c8c-93c9-e683937a3853").unwrap();
+//        let json0 = String::from ("{\"value:\":0}");
+//        let json1 = String::from ("{\"value:\":1}");
+//        let json2 = String::from ("{\"value:\":2}");
+//        let json3 = String::from ("{\"value:\":3}");
+//        let json4 = String::from ("{\"value:\":4}");
+//        let json5 = String::from ("{\"value:\":5}");
+//        let name0 = String::from("table0");
+//        let name1 = String::from("table1");
+//        let mut storage = Storage::new();
+//        assert_eq!(None, storage.get (&name0, &uuid0));
+//        assert_eq!(0, storage.count(&name0));
+//        assert_eq!(0, storage.table_count());
+//        storage.set (&name0, &uuid0, json0.clone());
+//        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
+//        assert_eq! (1, storage.count (&name0));
+//        assert_eq!(None, storage.get (&name1, &uuid1));
+//        assert_eq!(0, storage.count(&name1));
+//        assert_eq!(1, storage.table_count());
+//        storage.set (&name0, &uuid1, json1.clone());
+//        storage.set (&name1, &uuid2, json2.clone());
+//        storage.set (&name1, &uuid3, json3.clone());
+//        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
+//        assert_eq!(json1, storage.get (&name0, &uuid1).unwrap());
+//        assert_eq! (2, storage.count (&name0));
+//        assert_eq!(json2, storage.get (&name1, &uuid2).unwrap());
+//        assert_eq!(json3, storage.get (&name1, &uuid3).unwrap());
+//        assert_eq!(2, storage.count(&name1));
+//        assert_eq!(2, storage.table_count());
+//        storage.set (&name0, &uuid1, json4.clone());
+//        storage.set (&name1, &uuid3, json5.clone());
+//        assert_eq!(json0, storage.get (&name0, &uuid0).unwrap());
+//        assert_eq!(json4, storage.get (&name0, &uuid1).unwrap());
+//        assert_eq! (2, storage.count (&name0));
+//        assert_eq!(json2, storage.get (&name1, &uuid2).unwrap());
+//        assert_eq!(json5, storage.get (&name1, &uuid3).unwrap());
+//        assert_eq!(2, storage.count(&name1));
+//        assert_eq!(2, storage.table_count());
+//    }
 }
